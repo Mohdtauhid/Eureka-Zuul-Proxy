448c53bd4fc79cd935238f032404cc67c383b88a
Mohdtauhid <mohdtauhid445577@gmail.com>
2021-02-22 00:04:18 +0530

    first commit

---
+++ employee/src/main/java/com/api/employee/controllers/TestController.java
@@ -0,0 +1,33 @@
+package com.api.employee.controllers;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.employee.model.Employee;
+
+@RestController
+@RequestMapping("/api/v1/employee")
+public class TestController 
+{
+
+	private Employee emp=new Employee();
+	
+	@GetMapping()
+	public  ResponseEntity<Employee> firstPage() 
+	{
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		 return ResponseEntity.status(HttpStatus.OK).body(emp); 
+	}
+}
---
+++ zuul-server/src/test/java/com/api/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.api.zuulserver;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
